39cd880ddfb386eac9fc45fc1d7cc85e15e46823
swaroopn96 <swaroopn96@gmail.com>
2021-10-11 22:49:33 +0530

    DbContextCreation

---
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        //This Category is the name of Table we want to create
+        public DbSet<Category> Category { get; set; }
+    }
+}
