e6e11ebda6e2606c92c69bbf45b4a869f95a1cbf
swaroopn96 <swaroopn96@gmail.com>
2021-10-11 19:26:11 +0530

    Initial commit

---
+++ Ecommerce/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+
+        [Key] //This key makes this Id primary and identity
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
