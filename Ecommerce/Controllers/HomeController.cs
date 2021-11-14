cd6e539d8865788aaa8b63005b19bc881f5ee37b
swaroopn96 <swaroopn96@gmail.com>
2021-11-14 23:14:49 +0530

    Remove From Cart functionality added

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -37,12 +37,28 @@ namespace Ecommerce.Controllers
 
         public IActionResult Details(int id)
         {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstants.SessionCart);
+            }
+
             DetailsVM detailsVM = new DetailsVM()
             {
                 Product = _db.Products.Include(x => x.Category).Include(x => x.ApplicationType).Where(x => x.Id == id)
                 .FirstOrDefault(),
                 ExistsInCart = false
             };
+
+            foreach(var item in shoppingCartList)
+            {
+                if(item.ProductId==id)
+                {
+                    detailsVM.ExistsInCart = true;
+                }
+            }
+
             return View(detailsVM);
         }
 
@@ -59,6 +75,28 @@ namespace Ecommerce.Controllers
             HttpContext.Session.Set(WebConstants.SessionCart, shoppingCartList);
             return RedirectToAction(nameof(Index)); //if we use name of we get intellisence and chances of error is avoided 
         }
+
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WebConstants.SessionCart);
+            }
+
+            var itemToRemove = shoppingCartList.FirstOrDefault(x => x.ProductId == id);
+            if(itemToRemove!=null)
+            {
+                shoppingCartList.Remove(itemToRemove);
+            }
+
+            HttpContext.Session.Set(WebConstants.SessionCart, shoppingCartList);
+
+            return RedirectToAction(nameof(Index)); //if we use name of we get intellisence and chances of error is avoided 
+        }
+
 
 
         public IActionResult Privacy()
